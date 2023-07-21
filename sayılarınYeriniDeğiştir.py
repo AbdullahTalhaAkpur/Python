0313c5948d940d509323b07944bdb781e0344acb
AbdullahTalhaAkpur <114619517+AbdullahTalhaAkpur@users.noreply.github.com>
2023-07-21 23:05:04 +0300

    Add files via upload

---
+++ sayılarınYeriniDeğiştir.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 20 17:47:13 2023
+
+@author: Abdullah
+"""
+
+x = 10
+y = 20 
+
+x,y = y,x
+
+# temp = x
+# x = y
+# y = temp 
+
+print("x = "  +  str(x))
+print("y = "  +  str(y))
