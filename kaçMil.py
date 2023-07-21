f0ba9815133d5fe0f97e015af15763225902a136
AbdullahTalhaAkpur <114619517+AbdullahTalhaAkpur@users.noreply.github.com>
2023-07-21 23:04:39 +0300

    Add files via upload

---
+++ kaçMil.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 20 18:00:43 2023
+
+@author: Abdullah
+"""
+
+donusumOrani = 0.621371192
+km = int(input("Kaç km ?"))
+
+mil = km * donusumOrani
+print(str(km) +" Km =  "+str(mil)+" mil eder")
