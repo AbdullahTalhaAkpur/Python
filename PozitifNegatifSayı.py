c17fe92101eaa935cecf97d9e9c84d004c9e6dda
AbdullahTalhaAkpur <114619517+AbdullahTalhaAkpur@users.noreply.github.com>
2023-07-22 23:43:03 +0300

    Add files via upload

---
+++ PozitifNegatifSayı.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 22 17:51:39 2023
+
+@author: Abdullah
+"""
+
+sayi = int(input("Sayı giriniz : "))
+
+if sayi > 0:
+    print("Pozitif")
+elif sayi == 0:
+    print("Sıfır")
+else:
+    print("Negatif")     
