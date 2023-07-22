63f584fa1b67e97ca24ee40dfdf433f2c115fcc2
AbdullahTalhaAkpur <114619517+AbdullahTalhaAkpur@users.noreply.github.com>
2023-07-22 23:40:26 +0300

    Add files via upload

---
+++ kelimeSıralamaca.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 22 20:22:32 2023
+
+@author: Abdullah
+"""
+
+cumle = "bugün hava çok güzel"
+
+kelimeler = cumle.split()
+
+kelimeler.sort()
+
+print(kelimeler)
