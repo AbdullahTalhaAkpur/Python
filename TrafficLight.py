c8545f81bd7bd095c9963783560e050b017685c8
AbdullahTalhaAkpur <114619517+AbdullahTalhaAkpur@users.noreply.github.com>
2023-07-22 23:42:06 +0300

    Add files via upload

---
+++ TrafficLight.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 22 17:31:54 2023
+
+@author: Abdullah
+"""
+
+lights = ["red","yellow","green"]
+
+currentLight = lights[1]
+
+print(currentLight)
+
+if currentLight == "red":
+    print("STOP")    
+elif currentLight == "yellow":
+    print("READY")       
+else:
+    print("GO")
+            
+    
